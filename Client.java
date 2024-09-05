a51e1766e334b776d2ccba76d99d0a4a157ac9b7
Kjn22 <kavyajain9163@gmail.com>
2024-09-05 19:35:06 +0530

    Third Commit

--- Client.java
+++ Client.java
@@ -19,11 +19,13 @@ public class Client implements ActionListener {
 
     Client() {
         f.setLayout(null);
+        
         JPanel p1 = new JPanel();
         p1.setBackground(new Color(7, 94, 84));
         p1.setBounds(0, 0, 450, 70);
         p1.setLayout(null);
         f.add(p1);
+
         ImageIcon i1 = new ImageIcon(getClass().getResource("icons/3.png"));
         Image i2 = i1.getImage().getScaledInstance(25, 25, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
@@ -35,47 +37,56 @@ public class Client implements ActionListener {
                 System.exit(0);
             }
         });
+
         ImageIcon i4 = new ImageIcon(getClass().getResource("icons/user.png"));
         Image i5 = i4.getImage().getScaledInstance(60, 40, Image.SCALE_DEFAULT);
         ImageIcon i6 = new ImageIcon(i5);
         JLabel profile = new JLabel(i6);
         profile.setBounds(40, 10, 50, 50);
         p1.add(profile);
+
         ImageIcon i7 = new ImageIcon(getClass().getResource("icons/video.png"));
         Image i8 = i7.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
         ImageIcon i9 = new ImageIcon(i8);
         JLabel video = new JLabel(i9);
         video.setBounds(300, 20, 30, 30);
         p1.add(video);
+
         ImageIcon i10 = new ImageIcon(getClass().getResource("icons/phone.png"));
         Image i11 = i10.getImage().getScaledInstance(35, 30, Image.SCALE_DEFAULT);
         ImageIcon i12 = new ImageIcon(i11);
         JLabel phone = new JLabel(i12);
         phone.setBounds(360, 20, 35, 30);
         p1.add(phone);
+
         ImageIcon i13 = new ImageIcon(getClass().getResource("icons/3icon.png"));
         Image i14 = i13.getImage().getScaledInstance(10, 25, Image.SCALE_DEFAULT);
         ImageIcon i15 = new ImageIcon(i14);
         JLabel morevert = new JLabel(i15);
         morevert.setBounds(420, 20, 10, 25);
         p1.add(morevert);
+
         JLabel name = new JLabel("Client");
         name.setBounds(110, 15, 100, 20);
         name.setForeground(Color.WHITE);
         name.setFont(new Font("SAN_SERIF", Font.BOLD, 15));
         p1.add(name);
+
         JLabel status = new JLabel("Active Now");
         status.setBounds(110, 35, 100, 20);
         status.setForeground(Color.WHITE);
         status.setFont(new Font("SAN_SERIF", Font.BOLD, 15));
         p1.add(status);
+
         a1 = new JPanel();
         a1.setBounds(5, 75, 440, 570);
         f.add(a1);
+
         text = new JTextField();
         text.setBounds(5, 655, 310, 40);
         text.setFont(new Font("SAN_SERIF", Font.PLAIN, 15));
         f.add(text);
+
         JButton send = new JButton("Send");
         send.setBounds(320, 655, 125, 40);
         send.setBackground(new Color(7, 94, 84));
@@ -83,6 +94,7 @@ public class Client implements ActionListener {
         send.addActionListener(this);
         send.setFont(new Font("SAN_SERIF", Font.PLAIN, 15));
         f.add(send);
+
         f.setSize(450, 700);
         f.setLocation(800, 25);
         f.getContentPane().setBackground(Color.WHITE);
@@ -100,6 +112,7 @@ public class Client implements ActionListener {
             vertical.add(right);
             vertical.add(Box.createVerticalStrut(15));
             a1.add(vertical, BorderLayout.PAGE_START);
+
             dout.writeUTF(out);
             text.setText("");
             f.repaint();
@@ -119,6 +132,7 @@ public class Client implements ActionListener {
         output.setOpaque(true);
         output.setBorder(new EmptyBorder(15, 15, 15, 50));
         panel.add(output);
+        
         Calendar cal = Calendar.getInstance();
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
         JLabel time = new JLabel();
@@ -145,6 +159,7 @@ public class Client implements ActionListener {
                 f.validate();
             }
         } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
